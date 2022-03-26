fff751188d3d0b76c8f08bd20c36984a46cefc02
Achmad Zulfikar <46018794+zulfikarozil@users.noreply.github.com>
2022-03-26 13:47:44 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ date_default_timezone_set("Asia/Jakarta");
 | a PHP script and you can easily do that on your own.
 |
 */
-// $config['base_url'] = '';
+// $config['base_url'] = 'https://zulfikarozil.github.io/simpelbpn/';
 $root = (isset($_SERVER["HTTPS"]) ? "https://" : "http://") . $_SERVER["HTTP_HOST"];
 $root .= str_replace(basename($_SERVER["SCRIPT_NAME"]), "", $_SERVER["SCRIPT_NAME"]);
 $config["base_url"] = $root;
